daa0d5eacba08e4c4142ac2048e16ace73665666
Adrian Irving-Beer <wisq@wisq.net>
2020-05-31 16:35:32 -0400

    Switch to upstream rcon.

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule Mcscripts.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       # {:rcon, "~> 0.3.0"},
-      {:rcon, git: "https://github.com/wisq/elixir-rcon.git"},
+      {:rcon, github: "avitex/elixir-rcon"},
       {:dogstatsd, "0.0.3"},
       {:poison, "~> 3.1"}
     ]
